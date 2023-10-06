fc1baa2b2e094f89976fd9eef8f794f1efecdb34
Jelle Sjollema <j.sjollema@ma-web.nl>
2023-10-06 16:15:34 +0200

    centroid 2

--- Assets/Centroid/CentroidManager.cs
+++ Assets/Centroid/CentroidManager.cs
@@ -6,17 +6,47 @@ public class CentroidManager : MonoBehaviour
 {
     public DragablePoint A;
     public DragablePoint B;
+    public DragablePoint C;
+
     public GameObject AB;
+    public GameObject AC;
+    public GameObject BC;
+
     private LineRenderer lAB;
+    private LineRenderer lAC;
+    private LineRenderer lBC;
+
+    private LinearFunction fAB = new LinearFunction(1,0);
+    private LinearFunction fBC = new LinearFunction(1,0);
+    private LinearFunction fAC  = new LinearFunction(1,0);
+
+    public GameObject hAB;
+    public GameObject hAC;
 
     void Start()
     {
         lAB = AB.GetComponent<LineRenderer>();
+        lAC = AC.GetComponent<LineRenderer>();
+        lBC = BC.GetComponent<LineRenderer>();
+
     }
 
     void Update()
     {
-        lAB.SetPosition(0, new Vector3(-10, -10, 0));
-        lAB.SetPosition(1, new Vector3(10, 10, 0));  
+        fAB.LineTroughTwoPoint(A.transform.position, B.transform.position);
+        lAB.SetPosition(0, new Vector3(-10, fAB.getY(-10), 0));
+        lAB.SetPosition(1, new Vector3(10, fAB.getY(10), 0));
+
+        fAC.LineTroughTwoPoint(A.transform.position, C.transform.position);
+        lAC.SetPosition(0, new Vector3(-10, fAC.getY(-10), 0));
+        lAC.SetPosition(1, new Vector3(10, fAC.getY(10), 0));
+
+        fBC.LineTroughTwoPoint(B.transform.position, C.transform.position);
+        lBC.SetPosition(0, new Vector3(-10, fBC.getY(-10), 0));
+        lBC.SetPosition(1, new Vector3(10, fBC.getY(10), 0));
+
+        //hAB.transform.position = new Vector3((A.transform.position.x + B.transform.position.x) / 2, (A.transform.position.y + B.transform.position.y) / 2, 0);
+        hAB.transform.position = (A.transform.position + B.transform.position)/2;
+        hAC.transform.position = new Vector3((A.transform.position.x + C.transform.position.x) / 2, (A.transform.position.y + C.transform.position.y) / 2, 0);
     }
 }
